d71d3b70a575d04fbba08f358b17546624e42d1d
mctashuo <mctashuo@gmail.com>
2018-07-17 20:44:16 +0800

    单链表公共结点查找

--- ch01/FindFirstSameNode.cpp
+++ ch01/FindFirstSameNode.cpp
@@ -25,42 +25,60 @@ void print(SNode * head) {
     cout<<endl;
 }
 
-void Partition(SNode * head,int val) {
-    SNode * p1 = new SNode(0);
-    SNode *p2 = new SNode(0);
-    SNode *p1Head = p1;
-    SNode *p2Head = p2;
-    SNode * pHead = head->pNext;
-
-    while(pHead) {
-        if(pHead->value >= val) {
-            p2->pNext = pHead; 
-            p2 = p2->pNext;
-        }else {
-            p1->pNext = pHead;
-            p1 = p1->pNext;
-        }
-        pHead = pHead->pNext;
+int CalculatorLength(SNode * head) {
+    int count = 0;
+    while(head) {
+        count++;
+        head = head->pNext;
     }
-    p1->pNext = p2Head->pNext;
-    p2->pNext = NULL;
-    head->pNext = p1Head->pNext;
-    delete p1Head;
-    delete p2Head;
+    return count;
+}
+int FindFirstSameNode(SNode * head1,SNode * head2) {
+    int p1Len = CalculatorLength(head1->pNext);
+    int p2Len = CalculatorLength(head2);
+    
+    SNode * p1 = head1->pNext;
+    SNode * p2 = head2;
+
+    int len = p1Len - p2Len;
+    
+    if(p1Len < p2Len) {
+        SNode * temp = p1;
+        p1 = p2;
+        p2 = temp;
+        len = -len;
+    }
+    while(len--) {
+        p1 = p1->pNext;
+    }
+
+    while(p1) {
+        if(p1 == p2) 
+            return p1->value;
+
+        p1 = p1->pNext;
+        p2 = p2->pNext;
+    }
+
+    return 0;
+
 
 }
+
 int main() {
     SNode * head = new SNode(0);
-    //int data[] = {2,3,3,5,7,8,8,8,9,9,30};
-    //int size = sizeof(data) / sizeof(int);
+    int data[] = {2,3,3,5,7,8,8,8,9,9,30};
+    int size = sizeof(data) / sizeof(int);
     for(int i = 10;i >= 0;i--) {
-        SNode * p = new SNode(rand() % 10);
+        SNode * p = new SNode(data[i]);
         p->pNext = head->pNext;
         head->pNext = p;
     }
+    SNode * head2;
+    head2 = head->pNext->pNext->pNext;
     print(head);
-    Partition(head,5);
-    print(head);
+    print(head2);
+    cout<<FindFirstSameNode(head,head2)<<endl;
     destory(head);
     return 0;
 }
