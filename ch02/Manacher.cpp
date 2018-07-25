5825bdeecadee9aa28c9402d316c48e5ed7b987e
mctashuo <mctashuo@gmail.com>
2018-07-25 20:20:08 +0800

    Manacher算法(有错误暂时没改好)

---
+++ ch02/Manacher.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+//给字符串添加gap
+char * addGap(const char *a, int size) {
+    char *b = new char(2 * size +2);
+    b[0] = '$';
+    for(int i = 0;i < size;i++) {
+        b[2 + i * 2] = a[i];
+        b[i * 2 + 1] = '#'; 
+    }
+    b[2 * size + 1] = '#';
+    return b;
+
+}
+void Print(int * a,int size) {
+    for(int i = 0;i < size;i++) {
+        cout<<a[i]<<"\t";
+    }
+    cout<<endl;
+}
+void Manacher(const char * a,int size) {
+    const char * head = a;
+    int P[size];
+    P[0] = 1;
+    int mx,id;
+    int i = 1;
+    id = 0;
+    mx = 1;
+
+    a++;
+    while(*a) {
+        if(mx > i) {
+            mx - i > P[2 * id - i] ? P[i] = P[2*id-1] : P[i] = mx - i; 
+        }
+        else {
+            P[i] = 1;
+        }
+        for(;a[i + P[i] ] == a[i-P[i]];P[i]++);
+        
+        if(mx < i + P[i]) {
+            mx = i+P[i];
+            id = i;
+        }
+        a++;
+    }
+    Print(P,size);
+}
+int main() {
+    const char * a = "12212321";
+    char * b = addGap(a,strlen(a));
+    cout<<b<<endl;
+    Manacher(b,2 * strlen(a) + 1);
+    delete [] b;
+    return 0;
+}
